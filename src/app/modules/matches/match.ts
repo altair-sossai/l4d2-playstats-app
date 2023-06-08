0c975d3d56b6d44703920274031da708fb4c4405
Altair Sossai <altairsossai@gmail.com>
2023-06-08 11:55:18 -0300

    WIP

--- src/app/modules/matches/match.ts
+++ src/app/modules/matches/match.ts
@@ -1,4 +1,3 @@
-import { PlayerName } from "../l4d2-play-stats/player-name";
 
 export interface Match {
     matchStart: Date;
@@ -11,5 +10,26 @@ export interface Match {
 
 export interface Team {
     score: number;
-    players: PlayerName[];
+    players: Player[];
+    common: number;
+    siKilled: number;
+    siDamage: number;
+    dmgTank: number;
+}
+
+export interface Player {
+    steamId: string | null;
+    communityId: string | null;
+    steam3: string | null;
+    profileUrl: string | null;
+    index: number;
+    name: string | null;
+    common: number;
+    commonPercentage: number;
+    siKilled: number;
+    siKilledPercentage: number;
+    siDamage: number;
+    siDamagePercentage: number;
+    dmgTank: number;
+    dmgTankPercentage: number;
 }
