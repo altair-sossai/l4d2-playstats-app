b4060eb44f5188f7f12a8422e34736081f619369
Altair Sossai <altairsossai@gmail.com>
2023-06-12 18:02:27 -0300

    Added new stats

--- src/app/modules/matches/match.ts
+++ src/app/modules/matches/match.ts
@@ -1,4 +1,3 @@
-
 export interface Match {
     matchStart: Date;
     matchEnd: Date;
@@ -11,10 +10,23 @@ export interface Match {
 export interface Team {
     score: number;
     players: Player[];
+    died: number;
+    incaps: number;
+    dmgTaken: number;
     common: number;
     siKilled: number;
     siDamage: number;
+    tankDamage: number;
+    rockEats: number;
+    witchDamage: number;
+    skeets: number;
+    levels: number;
+    crowns: number;
+    ffGiven: number;
+    dmgTotal: number;
     dmgTank: number;
+    dmgSpit: number;
+    hunterDpDmg: number;
 }
 
 export interface Player {
@@ -24,12 +36,38 @@ export interface Player {
     profileUrl: string | null;
     index: number;
     name: string | null;
+    died: number;
+    diedPercentage: number;
+    incaps: number;
+    incapsPercentage: number;
+    dmgTaken: number;
+    dmgTakenPercentage: number;
     common: number;
     commonPercentage: number;
     siKilled: number;
     siKilledPercentage: number;
     siDamage: number;
     siDamagePercentage: number;
+    tankDamage: number;
+    tankDamagePercentage: number;
+    rockEats: number;
+    rockEatsPercentage: number;
+    witchDamage: number;
+    witchDamagePercentage: number;
+    skeets: number;
+    skeetsPercentage: number;
+    levels: number;
+    levelsPercentage: number;
+    crowns: number;
+    crownsPercentage: number;
+    ffGiven: number;
+    ffGivenPercentage: number;
+    dmgTotal: number;
+    dmgTotalPercentage: number;
     dmgTank: number;
     dmgTankPercentage: number;
+    dmgSpit: number;
+    dmgSpitPercentage: number;
+    hunterDpDmg: number;
+    hunterDpDmgPercentage: number;
 }
